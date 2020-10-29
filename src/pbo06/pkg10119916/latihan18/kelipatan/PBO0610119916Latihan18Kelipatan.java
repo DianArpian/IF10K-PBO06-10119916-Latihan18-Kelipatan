903b2c04034fc45989fd357339c58d13f70b5284
DianArpian <>
2020-10-29 18:15:00 +0700

    Kelipatan

---
+++ src/pbo06/pkg10119916/latihan18/kelipatan/PBO0610119916Latihan18Kelipatan.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo06.pkg10119916.latihan18.kelipatan;
+
+/**
+ *
+ * @author 
+ * NAMA     : DIAN ARPIAN
+ * NIM      : 10119916
+ * KELAS    : IF10K
+ * Deskripsi Program :Kelipatan
+ */
+public class PBO0610119916Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float bil;
+            for (bil=(float) 3.5; bil<=35; bil+=3.5){
+                System.err.println(bil);
+            }
+    }
+    
+}
